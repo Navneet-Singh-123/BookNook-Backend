6d6ce7399829ac13a475a277e073f82158113e63
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-11-25 18:57:20 +0530

    Product delete and update completed

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {create} = require('../Controllers/product.js')
+const {create, productById, read, remove, update} = require('../Controllers/product.js')
 
 const {
     requireSignin, 
@@ -11,9 +11,13 @@ const {
 
 const {userById} = require("../Controllers/user")
 
+router.get('/product/:productId', read);
 router.post('/product/create/:userId',requireSignin, isAuth, isAdmin, create)
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove)
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update)
 
 router.param('userId', userById)
+router.param('productId', productById)
 
 
 
